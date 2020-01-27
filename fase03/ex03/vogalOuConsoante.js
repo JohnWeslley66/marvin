6770d8bbbcf08f0c4459faadfb6104fde4eb6702
JohnWeslley66 <60019449+JohnWeslley66@users.noreply.github.com>
2020-01-27 20:53:05 -0300

    ex003

---
+++ fase03/ex03/vogalOuConsoante.js
@@ -0,0 +1,10 @@
+function vogalOuConsoante(letra){
+   var mi = letra.toLowerCase()
+        if(mi == "a"||mi === "e"||mi == "i"||mi == "o"||mi == "u")
+    return('Vogal')
+    
+    else
+    return ('Consoante')
+}
+
+
