42f924200c0ba3912d5f350aa41309d8aea1c556
JohnWeslley66 <60019449+JohnWeslley66@users.noreply.github.com>
2020-01-29 19:31:25 -0300

    ex001

--- fase04/ex00/entreNumeros.js
+++ fase04/ex00/entreNumeros.js
@@ -1,9 +1,15 @@
-function entreNumeros(min, max){
-    
-    while(min <= max){
-        console.log(min)
-         min++
-    }
+function ateZero(num){
+    if(num < 0){
+       while(num < 0){
+           console.log(num)
+           num++}}
+           
+    if(num >= 0){
+        var i = 0
+        while(i <= num){
+            console.log(i)
+            i++
+        }}
 }
 
 
