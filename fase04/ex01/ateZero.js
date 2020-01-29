6205a6c5dc0ebe268fcb6c28856ba21d327f38d8
JohnWeslley66 <60019449+JohnWeslley66@users.noreply.github.com>
2020-01-29 19:25:28 -0300

    ex001

---
+++ fase04/ex01/ateZero.js
@@ -0,0 +1,14 @@
+function ateZero(num){
+    if(num < 0){
+       while(num <= 0){
+           console.log(num)
+           num++}}
+    if(num >= 0){
+        var i = 0
+        while(i <= num){
+            console.log(i)
+            i++
+        }}
+}
+
+
