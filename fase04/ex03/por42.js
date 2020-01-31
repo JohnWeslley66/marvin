ba3f6ad3dad042f9424e1f3b3668b5f565fe0645
JohnWeslley66 <60019449+JohnWeslley66@users.noreply.github.com>
2020-01-31 19:22:40 -0300

    ex003

--- fase04/ex03/por42.js
+++ fase04/ex03/por42.js
@@ -1,18 +1,24 @@
 function por42(num1, num2){
-    var num1 = num1 + 42
-    if(num1 > num2){
-        return false
-    }
-    
-    while ( num1 < num2 ){
-        
-        if ( num1 % 42 === 0){
-            return num1
-          
-            
-        }
-    
-    num1++
-    }
+    var num1 = num1+42
+   while ( num1 <= num2){
+       if ( num1 % 42 === 0){
+           return num1
+       }
+       num1++
+   }
+   while ( num1 < 0){
+       if (num1 >= num2){
+           console.log('Não encontrado')
+           return false
+           }
+        if( num1 <= num2){
+            if ( num1 % 42 === 0){
+                return num1
+       }
+       }
+        num1++
+   }
+   
+  
 }
 
