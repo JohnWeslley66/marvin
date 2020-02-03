97180e07e80e3e4549c901fd60c36ba963ad999c
JohnWeslley66 <60019449+JohnWeslley66@users.noreply.github.com>
2020-02-03 19:59:19 -0300

    ex0001

---
+++ fase05/ex01/penultimo.js
@@ -0,0 +1,11 @@
+function penultimo(array){
+    let cont = 0;
+    let penult = array.length - 2;
+    while(cont <= array.length){
+        if(cont === penult){
+            return array[cont]
+        }
+        cont++;
+    }
+}
+
